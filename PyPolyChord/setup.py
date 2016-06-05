34b7e7e7bb6b47fa7e2b286e8c2f30c0b60fc513
J. Michael Burgess <jmichaelburgess@gmail.com>
2016-06-05 11:47:28 +0200

    pypolchord natural install

--- PyPolyChord/setup.py
+++ PyPolyChord/setup.py
@@ -4,14 +4,15 @@ from distutils.core import setup, Extension
 lib_dir = '/usr/local/lib'#os.environ['LIB_DIR']
 
 pypolychord_module = Extension(
-        name= '_PyPolyChord',
+        name= 'pypolychord/_PyPolyChord',
         include_dirs = ['/usr/local/include'],
         library_dirs = [lib_dir],
-        libraries = ['chord','gfortran'],
-        sources=['_PyPolyChord.c']
+        libraries = ['chord'],
+        sources=['pypolychord/_PyPolyChord.c']
         )
 
 setup(
+    packages=['pypolychord'],
     name = 'PyPolyChord',
     version = '1.0',
     description = 'Run PolyChord with Python',
